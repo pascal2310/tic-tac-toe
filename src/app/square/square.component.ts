3d97646c402ff0c1b13cb014a31487eff0b5ed14
pascal2310 <pascal_96@live.nl>
2020-10-10 20:35:21 +0200

    tictactoe game

---
+++ src/app/square/square.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  template: `
+  <button nbButton *ngIf="!value" >{{value}}</button>
+  <button nbButton hero status="success" *ngIf="value == 'X'">{{ value }}</button>
+  <button nbButton hero status="info" *ngIf="value == 'O'">{{ value }}</button>
+  `
+  ,
+  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
+})
+export class SquareComponent implements OnInit {
+
+  random = Math.random();
+
+  @Input() value : 'X' | 'O';
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
